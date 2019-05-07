18549c41ac85b3402238563a6a9204e41dc6dc2b
Sharma <kusharma@amazon.com>
2019-05-06 21:56:42 -0400

    Updating definition of AgentSessionTerminationException.java

--- src/main/java/com/power/dialer/exception/AgentSessionTerminationException.java
+++ src/main/java/com/power/dialer/exception/AgentSessionTerminationException.java
@@ -1,5 +1,8 @@
 package com.power.dialer.exception;
 
+/*
+ * Error thrown when agent tries to terminate the session when in ENGAGED status
+ */
 public class AgentSessionTerminationException extends RuntimeException {
 
     public AgentSessionTerminationException(final String message) {
